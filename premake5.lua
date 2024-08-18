855e1af4aedf62a5bd21a17d619787d719a9a267
ggjorven <Jorbenvanderwal@gmail.com>
2024-08-18 21:14:07 +0200

    Fix GLFW warning.

--- premake5.lua
+++ premake5.lua
@@ -44,37 +44,63 @@ project "GLFW"
 			"src/osmesa_context.c"
 		}
 
-		defines 
-		{ 
+		defines
+		{
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
 	filter "system:linux"
+        systemversion "latest"
 		pic "On"
 
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
+        files
+        {
+            "src/xkb_unicode.c",
+            "src/posix_module.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
 			"src/posix_module.c",
-			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
 			"src/linux_joystick.c"
-		}
+        }
 
-		defines
-		{
-			"_GLFW_X11"
-		}
+        -- Check if system is running wayland
+        if os.getenv("WAYLAND_DISPLAY") then -- Note: Wayland has never been tested.
+            defines { "_GLFW_WAYLAND" }
+
+            files
+            {
+               "src/wl_init.c",
+               "src/wl_monitor.c",
+               "src/wl_window.c",
+            }
+
+         -- Else check if system is running x11
+        elseif os.getenv("DISPLAY") then
+            defines { "_GLFW_X11" }
+
+            files
+            {
+                "src/x11_init.c",
+                "src/x11_monitor.c",
+                "src/x11_window.c",
+                "src/glx_context.c",
+            }
+        else
+            print("[GLFW] [WARN]: Failed to query if display is X11 or Wayland. Defaulting to X11. If not on linux, ignore this warning.")
+
+            defines { "_GLFW_X11" }
+
+            files
+            {
+                "src/x11_init.c",
+                "src/x11_monitor.c",
+                "src/x11_window.c",
+                "src/xkb_unicode.c",
+            }
+        end
 
 	filter "system:macosx"
 		pic "On"
